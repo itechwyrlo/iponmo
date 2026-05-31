f2247f1895bfc6f2f4aee6d970f77664b1193fc1
Wyrlo Lerios <wyrlo.adeva.lerios@gmail.com>
2026-05-31 23:23:09 +0800

    config fix and logo

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
         background_color: '#0f0e17',
         display: 'standalone',
         icons: [
-          { src: '/logo192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/logo512.png', sizes: '512x512', type: 'image/png' },
+          { src: '/iponmo.png', sizes: '192x192', type: 'image/png' },
+          { src: '/iponmo.png', sizes: '512x512', type: 'image/png' },
         ],
       },
     }),
