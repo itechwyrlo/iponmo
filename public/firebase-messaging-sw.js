d95aef9e55b4a7250c767e9cdd8d5755b2210b49
Wyrlo Lerios <wyrlo.adeva.lerios@gmail.com>
2026-06-01 16:10:02 +0800

    Ntoification firbase fix

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,11 +6,11 @@ importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: 'VITE_FIREBASE_API_KEY_PLACEHOLDER',
-  authDomain: 'VITE_FIREBASE_AUTH_DOMAIN_PLACEHOLDER',
-  projectId: 'VITE_FIREBASE_PROJECT_ID_PLACEHOLDER',
-  messagingSenderId: 'VITE_FIREBASE_MESSAGING_SENDER_ID_PLACEHOLDER',
-  appId: 'VITE_FIREBASE_APP_ID_PLACEHOLDER',
+  apiKey: 'AIzaSyC4otpQQyek6VXVO4IYI1ynMR4j58mV3GM',
+  authDomain: 'iponmo-a3dd5.firebaseapp.com',
+  projectId: 'iponmo-a3dd5',
+  messagingSenderId: '529677416614',
+  appId: '1:529677416614:web:a27c43fd4836c29ed2dc67',
 });
 
 const messaging = firebase.messaging();
